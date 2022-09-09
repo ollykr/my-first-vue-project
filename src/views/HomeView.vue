97e989316cc9681aa9b93a202f3aa7f820601be0
ollykr <okremianski@gmail.com>
2022-09-09 15:42:41 -0400

    Cleanup - step-three

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,31 @@
 <script setup>
-
 import { onMounted, ref } from "vue";
 
 let textarea = ref(null);
 
+function onTabPress(e) {
+  // Give me target of the event - i.e textarea node
+  let textarea = e.target;
+  // get caret position/selection
+  let val = textarea.value,
+    start = textarea.selectionStart,
+    end = textarea.selectionEnd;
 
-function onKeyDown(e) {
-    let t = textarea.value;
-    // tab was pressed
-    if (e.keyCode === 9) {
-      // get caret position/selection
-      let val = t.value,
-        start = t.selectionStart,
-        end = t.selectionEnd;
+  // set textarea value to: text before caret + tab + text after caret
 
-      // set textarea value to: text before caret + tab + text after caret
+  textarea.value = val.substring(0, start) + "\t" + val.substring(end);
 
-      t.value = val.substring(0, start) + "\t" + val.substring(end);
-
-      // put caret at right position again
-      t.selectionStart = t.selectionEnd = start + 1;
-      e.preventDefault();
-    }
-};
+  // put caret at right position again
+  textarea.selectionStart = textarea.selectionEnd = start + 1;
+}
 </script>
 <template>
   <main>
     <form action="">
       <!-- use "ref" to access any DOM element -->
+      <!-- .prevent is  the same as e.preventDefault() -->
       <textarea
-        ref="textarea"
-        @keydown="onKeyDown"
+        @keydown.tab.prevent="onTabPress"
         style="width: 100%; height: 300px"
       >
   Hi There!
