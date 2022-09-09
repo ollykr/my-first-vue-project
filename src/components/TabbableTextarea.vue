28a01ebc6d4c1270c495a930163ff0febef49093
ollykr <okremianski@gmail.com>
2022-09-09 15:53:45 -0400

    Abstract away logic by using dedicated component file

---
+++ src/components/TabbableTextarea.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- .prevent is  the same as e.preventDefault() -->
+  <textarea
+    @keydown.tab.prevent="onTabPress">
+</textarea
+  >
+</template>
+
+<script setup>
+function onTabPress(e) {
+  let textarea = e.target;
+
+  let val = textarea.value,
+    start = textarea.selectionStart,
+    end = textarea.selectionEnd;
+
+  textarea.value = val.substring(0, start) + "\t" + val.substring(end);
+
+  textarea.selectionStart = textarea.selectionEnd = start + 1;
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,10 @@
 <script setup>
-import { onMounted, ref } from "vue";
-
-let textarea = ref(null);
-
-function onTabPress(e) {
-  // Give me target of the event - i.e textarea node
-  let textarea = e.target;
-  // get caret position/selection
-  let val = textarea.value,
-    start = textarea.selectionStart,
-    end = textarea.selectionEnd;
-
-  // set textarea value to: text before caret + tab + text after caret
-
-  textarea.value = val.substring(0, start) + "\t" + val.substring(end);
-
-  // put caret at right position again
-  textarea.selectionStart = textarea.selectionEnd = start + 1;
-}
+import TabbableTextarea from "@/components/TabbableTextarea.vue";
 </script>
 <template>
   <main>
     <form action="">
-      <!-- use "ref" to access any DOM element -->
-      <!-- .prevent is  the same as e.preventDefault() -->
-      <textarea
-        @keydown.tab.prevent="onTabPress"
-        style="width: 100%; height: 300px"
-      >
-  Hi There!
-</textarea
-      >
+      <TabbableTextarea style="width: 100%; height: 300px;" />
     </form>
   </main>
 </template>
