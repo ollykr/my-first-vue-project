6b7a8bd77fdac79f5d7df8d906e972198c8ae596
ollykr <okremianski@gmail.com>
2022-09-09 16:10:03 -0400

    Emit the event - composition api style

--- src/components/TabbableTextarea.vue
+++ src/components/TabbableTextarea.vue
@@ -1,8 +1,13 @@
 <script setup>
 // defineProps macro
 defineProps({
-modelValue: String
+  modelValue: String,
 });
+
+// Composition API for emits
+// It is a good practice to declare it here
+let emit = defineEmits(["update:modelValue"]);
+
 function onTabPress(e) {
   let textarea = e.target;
 
@@ -14,10 +19,22 @@ function onTabPress(e) {
 
   textarea.selectionStart = textarea.selectionEnd = start + 1;
 }
+
+function update(e) {
+  // in Options API we emit like this - this.$emit('update:modelValue', 'whateverstringis')
+  // with <script setup> we can't do above
+
+  // Composition API
+  emit("update:modelValue", e.target.value);
+}
 </script>
 
 <template>
   <!-- .prevent is  the same as e.preventDefault() -->
   <!-- Use "modelValue" prop via v-text="modelValue" -->
-  <textarea @keydown.tab.prevent="onTabPress" v-text="modelValue" />
+  <textarea
+    @keydown.tab.prevent="onTabPress"
+    @keyup="update"
+    v-text="modelValue"
+  />
 </template>
