ba2335870165ebdc450809a60202f78ceeac9a53
ollykr <okremianski@gmail.com>
2022-10-07 12:30:36 -0400

    Complete working with react data without drilling down to children components

--- src/components/Quiz/Quiz.vue
+++ src/components/Quiz/Quiz.vue
@@ -1,5 +1,4 @@
 <template>
-<h1>{{ name }}</h1>
 <QuizHeader />
 
 <QuizQuestion />
@@ -15,23 +14,11 @@ import QuizQuestion from "@/components/Quiz/QuizQuestion.vue";
 
 import QuizFooter from "@/components/Quiz/QuizFooter.vue";
 
-import {provide, ref} from "vue";
+import {provide} from "vue";
 
-defineProps({ quiz: Object });
+// Quiz needs to be available in QuizFooterLinks nested child component
+let props = defineProps({ quiz: Object });
 
-// For reactive data
-let name = ref('John Doe');
+provide('quiz', props.quiz);
 
-// Component provides a value accessible by "children/grandchildren" components, we don't need to drill down to them
-// second paramether - name is not just "John Doe", it is a reactive object
-
-// The only place you can change a data is in parent - this is a rule, that's why we use an object as a second paramether
-// function 'changeName' is responsible for changing a name
-// name, means key and value are the same - 'name'
-// If we want to trigger a change , we just need to pass a reference to the function in a child
-provide('name', {
-    name,
-changeName: () => name.value = 'Changed'
-
-});
 </script>
--- src/components/Quiz/QuizFooterLinks.vue
+++ src/components/Quiz/QuizFooterLinks.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
-        <!-- When you click on me, I wanna call 'changeName' function from a parent - Quiz.vue -->
-        <button @click="changeName">{{ name }}</button>
+      <h5>{{ quiz.name }}</h5>
 
     <ul>
         <li><a href="#">Get a Job</a></li>
@@ -10,14 +9,10 @@
     </div>
 </template>
 <script setup>
-// Grab a value from Quiz.vue which a granddad of this component
 import {inject} from "vue";
 
-defineProps({ quiz: Object });
-// Accept parent's changeName function
-let { name, changeName } = inject('name');
 
-// Triggering a change set up on a parent via function here
+let { quiz } = inject('quiz');
 
 
 </script>
