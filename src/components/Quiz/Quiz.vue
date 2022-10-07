53e0712d3394eac743d6975545a15b65e4d67cf3
ollykr <okremianski@gmail.com>
2022-10-07 12:06:18 -0400

    Different options to deal with reactive data-option 1

--- src/components/Quiz/Quiz.vue
+++ src/components/Quiz/Quiz.vue
@@ -1,5 +1,5 @@
 <template>
-
+<h1>{{ name }}</h1>
 <QuizHeader />
 
 <QuizQuestion />
--- src/components/Quiz/QuizFooterLinks.vue
+++ src/components/Quiz/QuizFooterLinks.vue
@@ -17,4 +17,9 @@ defineProps({ quiz: Object });
 
 let name = inject('name');
 
+// Since the data is reactive , we can test it using a Timeout, a new name is reflected in a parent as well since it is a reactive data
+setTimeout(() => {
+    name.value = 'A New Name';
+}, 2000);
+
 </script>
